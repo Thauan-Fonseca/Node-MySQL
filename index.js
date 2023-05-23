3b483a401405bbfbf24f5a9dbe3b88f0af24b460
Thauan-Fonseca <126363662+Thauan-Fonseca@users.noreply.github.com>
2023-05-22 23:31:05 -0300

    subindo projeto

---
+++ index.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root", 
+    password: "senha",
+    database: "nodemysql"
+});
+
+db.connect((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log("conectado ao banco de dados mysql")
+})
+
+//inserindo dados
+
+app.post('/books', (req, res) => {
+    const {title, pages} = req.body
+    if(!title || !pages){
+        return res.status(422).json({msg: "Prencha todos os campos"});
+    }
+    const sql = `INSERT INTO books (title, pages) VALUES ("${title}", ${pages})`;
+    db.query(sql, (err, result) => {
+        if(err){
+            throw err
+        }
+        res.json({msg: "Livro cadastrado com sucesso!"})
+    })
+})
+
+// 3 -  Resgatando dados
+
+app.get('/books', (req, res) => {
+    const sql = "SELECT * FROM books";
+
+    db.query(sql, (err, result) => {
+        if(err){
+            throw err
+        }
+
+        res.json({books: result})
+    })
+})
+
+// 4 - Resgate de dados específicos
+
+app.get('/books/search', (req, res) => {
+    const {title} = req.query;
+    const sql = `SELECT * FROM books WHERE title LIKE '%${title}'`;
+
+    db.query(sql, (err, result) => {
+        if(err){
+            throw err;
+        }
+
+        res.json({books: result})
+    })
+
+})
+
+//5 - Editando dados
+
+app.patch('/books/:id', (req, res) => {
+    const {title, pages} = req.body;
+
+    const {id} = req.params;
+
+    const sqlcheck = `SELECT * FROM books WHERE id = ${id}`;
+
+    db.query(sqlcheck, (err, result) => {
+        if(err){
+            throw err;
+        }
+
+        if(result.length === 0){
+            res.status(404).json({message: 'Livro não encontrado!'})
+        } else {
+            const sql = `UPDATE books SET title = "${title}", pages = ${pages} where id = ${id}`;
+
+            db.query(sql, (err, result) => {
+                if(err){
+                    throw err;
+                }
+
+                res.json({message: "Livro atualizado com sucesso!"})
+            })
+        }
+    })
+});
+
+//5 - Deletando dados
+
+app.delete('/books/:id', (req, res) => {
+    const {id} = req.params;
+
+    const sqlcheck = `SELECT * FROM books WHERE id = ${id}`;
+
+    db.query(sqlcheck, (err, result) => {
+        if(err){
+            throw err;
+        }
+
+        if(result.length === 0){
+            res.status(404).json({message: 'Livro não encontrado!'})
+        } else {
+            const sql = `DELETE from books where id = ${id}`;
+
+            db.query(sql, (err, result) => {
+                if(err){
+                    throw err;
+                }
+
+                res.json({message: "Livro removido com sucesso!"})
+            })
+        }
+    })
+});
+
+//7 - pool connection
+
+const db1 = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "root", 
+    password: "senha",
+    database: "nodemysql"
+});
+
+
+app.listen((port), () => {
+    console.log(`servidor conectado na porta ${port}`);
+
+   
+})
