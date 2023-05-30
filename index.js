53cbdf41f8807f52e3a58504cf63c6308e634752
Thauan-Fonseca <126363662+Thauan-Fonseca@users.noreply.github.com>
2023-05-30 00:02:24 -0300

    adicionando assets

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors')
 
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(express.json());
 
 
