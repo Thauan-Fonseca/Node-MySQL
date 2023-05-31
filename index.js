6fb1a5f1093a5befe0817626a331475b4222391e
Thauan-Fonseca <126363662+Thauan-Fonseca@users.noreply.github.com>
2023-05-30 21:37:07 -0300

    Deploy do site

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const cors = require('cors')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json());
@@ -35,7 +35,7 @@ app.post('/books', (req, res) => {
         if(err){
             throw err
         }
-        res.json({msg: "Livro cadastrado com sucesso!"})
+        res.json({msg: "Livro cadastrado com sucesso!", id: result.insertId})
     })
 })
 
